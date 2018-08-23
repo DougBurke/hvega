6b23c2beb399f4324596f2f34797433ecb63882e
Nicolas Mattia <nicolas@nmattia.com>
2018-08-23 21:41:03 +0300

    Simplify nix-shell

--- nix/haskellPackages/overlay.nix
+++
@@ -1,6 +0,0 @@
-self: super:
-  { haskellPackages = super.haskellPackages.override
-    { overrides = haskellSelf: haskellSuper:
-      { shelly = self.haskell.lib.dontCheck haskellSuper.shelly; };
-    };
-  }
