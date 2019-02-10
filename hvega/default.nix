514f74c7f209037de9dfb7e5739eb63c55d44a1a
Douglas Burke <dburke.gw@gmail.com>
2019-02-10 11:13:57 -0500

    Udpate default.nix

--- hvega/default.nix
+++ hvega/default.nix
@@ -1,7 +1,7 @@
 { mkDerivation, aeson, base, stdenv, text, vector }:
 mkDerivation {
   pname = "hvega";
-  version = "0.1.0.0";
+  version = "0.2";
   src = ./.;
   libraryHaskellDepends = [ aeson base text vector ];
   homepage = "https://github.com/DougBurke/hvega";
