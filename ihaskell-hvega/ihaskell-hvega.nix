b87c601a63b8c777ba223c3de8e7f0b563e8efee
Douglas Burke <dburke.gw@gmail.com>
2019-07-29 18:44:43 -0400

    Update ihaskell-hvega nix files
    This also updates the version.json file but I do not think Ihaskell is
    working in this particular release.

---
+++ ihaskell-hvega/ihaskell-hvega.nix
@@ -0,0 +1,10 @@
+{ mkDerivation, aeson, base, hvega, ihaskell, stdenv, text }:
+mkDerivation {
+  pname = "ihaskell-hvega";
+  version = "0.2.0.3";
+  src = ./.;
+  libraryHaskellDepends = [ aeson base hvega ihaskell text ];
+  homepage = "https://github.com/DougBurke/hvega";
+  description = "IHaskell display instance for hvega types";
+  license = stdenv.lib.licenses.bsd3;
+}
