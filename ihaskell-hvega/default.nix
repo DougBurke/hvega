31aefdfb9dec932a73fe57d6826e0e5c5575c244
Douglas Burke <dburke.gw@gmail.com>
2020-10-18 22:29:38 -0400

    Bump ihaskell-hvega to 0.3.2.0 to support hvega 0.11

--- ihaskell-hvega/default.nix
+++ ihaskell-hvega/default.nix
@@ -1,2 +1,2 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc883" }:
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc884" }:
 nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./ihaskell-hvega.nix { }
