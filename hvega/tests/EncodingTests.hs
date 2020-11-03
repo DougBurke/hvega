db9516e7b94582bb00a183a36656bcb22fa363e4
Douglas Burke <dburke.gw@gmail.com>
2020-11-03 10:51:19 -0500

    Add encoding "null" test
    See https://github.com/vega/vega-lite/issues/6762 but we do not
    create a null, rather the empty object.

--- hvega/tests/EncodingTests.hs
+++ hvega/tests/EncodingTests.hs
@@ -13,6 +13,7 @@ import Graphics.Vega.VegaLite
 testSpecs :: [(String, VegaLite)]
 testSpecs = [ ("strokedashgroup", strokeDashGroup)
             , ("strokedashline", strokeDashLine)
+            , ("nullencoding", nullEncoding)
             ]
             
 
@@ -41,3 +42,26 @@ strokeDashLine =
                . strokeDash [ MName "predicted", MmType Nominal ]
                $ []
              ]
+
+-- See https://github.com/vega/vega-lite/issues/6762
+--
+-- Note: a "null" encoding here is with no arguments, which creates {}.
+-- We don't support creating null instead of {}.
+--
+nullEncoding :: VegaLite
+nullEncoding =
+  let dvals = dataFromColumns []
+              . dataColumn "a" (Strings ["A", "B", "D", "E", "E", "G", "H", "I"])
+              . dataColumn "b" (Numbers [28, 55, 91, 81, 81, 19, 87, 52])
+
+      lyr1 = [mark Bar []]
+      lyr2 = [mark Rule [], encoding (position X [] [])]
+      
+  in toVegaLite [ description "Can we null the encoding"
+                , dvals []
+                , encoding
+                  . position X [PName "a", PmType Nominal, PAxis [AxLabelAngle 0]]
+                  . position Y [PName "b", PmType Quantitative]
+                  $ []
+                , layer (map asSpec [lyr1, lyr2])
+                ]
