b8e33f5bec93b197f2e9209e5ae79db6c30a2455
Douglas Burke <dburke.gw@gmail.com>
2021-06-21 10:56:11 -0400

    Use the local not hackage hvega/ihaskell-hvega versions
    Should this be using an override instead, or something else?

--- default.nix
+++ default.nix
@@ -1,8 +1,24 @@
 let
   sources = import nix/sources.nix;
+  pkgs = import sources.nixpkgs {};
+
+  # Use the local hvega/ihaskell-hvega rather than the nixpkgs ones.
+  # I should probably be using an override.
+  #
+  hvegaSource = ./.;
+  hvegaDir = hvegaSource + "/hvega";
+  ihaskellvegaDir = hvegaSource + "/ihaskell-hvega";
+
+  gitignore = pkgs.nix-gitignore.gitignoreSourcePure [ ./.gitignore ];
+  ps = pkgs.haskellPackages;
+  hvega = ps.callCabal2nix "hvega" (gitignore hvegaDir) {};
+  ihaskell-hvega = ps.callCabal2nix "ihaskell-hvega" (gitignore ihaskellvegaDir) {};
+
 in import "${sources.IHaskell}/release.nix" {
   compiler = "ghc8104";
-  nixpkgs  = import sources.nixpkgs {};
-  packages = self: with self; [ hvega ihaskell-hvega ];
+  nixpkgs = pkgs;
+  packages = p: [ hvega ihaskell-hvega ];
+  # packages = p: [ hvega ihaskell-hvega p.aeson p.aeson-pretty p.Frames p.foldl p.formatting p.microlens ];
+  # packages = self: with self; [ hvega ihaskell-hvega ];
   # systemPackages = self: with self; [ graphviz ];
 }
