519dbfcace0fa0669d26f17323c2cac79a454176
Douglas Burke <dburke.gw@gmail.com>
2021-10-18 13:03:37 -0400

    Bump nix to ghc 8.10.7

--- default.nix
+++ default.nix
@@ -15,7 +15,7 @@ let
   ihaskell-hvega = ps.callCabal2nix "ihaskell-hvega" (gitignore ihaskellvegaDir) {};
 
 in import "${sources.IHaskell}/release.nix" {
-  compiler = "ghc8104";
+  compiler = "ghc8107";
   nixpkgs = pkgs;
   # packages = p: [ hvega ihaskell-hvega ];
   packages = p: [ hvega ihaskell-hvega p.aeson p.aeson-pretty p.Frames p.foldl p.formatting p.microlens ];
