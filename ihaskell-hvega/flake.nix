08fea349f364f5cd840bcdc1f171e38d45e547b5
Douglas Burke <dburke.gw@gmail.com>
2023-12-26 13:09:59 -0500

    bump for ihaskell version
    I can not get this to build with text 2.1 and ihaskell 0.11
    because of conflicts with other packages, so it is not clear
    if this is a useful change at this time.

--- ihaskell-hvega/flake.nix
+++ ihaskell-hvega/flake.nix
@@ -1,6 +1,6 @@
 {
   # inspired by: https://serokell.io/blog/practical-nix-flakes#packaging-existing-applications
-  description = "hvega: Create Vega-Lite visualizations (version 4) in Haskell";
+  description = "Allow vega visualizations to auto-display in IHaskell";
   inputs.nixpkgs.url = "nixpkgs";
   outputs = { self, nixpkgs }:
     let
@@ -14,16 +14,19 @@
     in
     {
       overlay = (final: prev: {
-        hvega = final.haskellPackages.callCabal2nix "hvega" ./. {};
+        ihaskell-hvega = final.haskellPackages.callCabal2nix "ihaskell-hvega" ./. {};
+        hvega = final.haskellPackages.callCabal2nix "../hvega" ./. {};
       });
       packages = forAllSystems (system: {
+         # do we need to set/override both?
          hvega = nixpkgsFor.${system}.hvega;
+         ihaskell-hvega = nixpkgsFor.${system}.ihaskell-hvega;
       });
-      defaultPackage = forAllSystems (system: self.packages.${system}.hvega);
+      defaultPackage = forAllSystems (system: self.packages.${system}.ihaskell-hvega);
       checks = self.packages;
       devShell = forAllSystems (system: let haskellPackages = nixpkgsFor.${system}.haskellPackages;
         in haskellPackages.shellFor {
-          packages = p: [self.packages.${system}.hvega];
+          packages = p: [self.packages.${system}.ihaskell-hvega];
           withHoogle = true;
           buildInputs = with haskellPackages; [
             haskell-language-server
@@ -32,12 +35,12 @@
           ];
         # Change the prompt to show that you are in a devShell
         shellHook = ''
-  echo -e "*** \e[1;32mWelcome to hvega\e[0m ***"
+  echo -e "*** \e[1;32mWelcome to ihaskell-hvega\e[0m ***"
   ghc --version
   cabal --version
   hlint --version
   echo -e ""
-  export PS1='hvega:\A \e[1;34m\w\e[0m '
+  export PS1='ihaskell-hvega:\A \e[1;34m\w\e[0m '
 	'';
         });
   };
