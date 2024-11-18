142d1b91578147399b1fa7e06bbc080e53aae4da
Douglas Burke <dburke.gw@gmail.com>
2024-11-18 15:53:59 -0500

    flake updates

--- flake.nix
+++ flake.nix
@@ -1,7 +1,13 @@
 {
   # inspired by: https://serokell.io/blog/practical-nix-flakes#packaging-existing-applications
-  description = "hvega: Create Vega-Lite visualizations (version 4) in Haskell";
-  inputs.nixpkgs.url = "nixpkgs";
+  description = "explore hvega and IHaskell";
+  
+  inputs = {
+    nixpkgs.url = "nixpkgs";
+    # IHaskell.url = "github:IHaskell/IHaskell";
+  };
+  
+  # outputs = { self, nixpkgs, IHaskell }:
   outputs = { self, nixpkgs }:
     let
       supportedSystems = [ "x86_64-linux" "x86_64-darwin" ];
@@ -14,31 +20,38 @@
     in
     {
       overlay = (final: prev: {
-        hvega = final.haskellPackages.callCabal2nix "hvega" ./. {};
+        hvega = final.haskellPackages.callCabal2nix "hvega" ./hvega {};
+        ihaskell-hvega = final.haskellPackages.callCabal2nix "ihaskell-hvega" ./ihaskell-hvega {};
       });
       packages = forAllSystems (system: {
          hvega = nixpkgsFor.${system}.hvega;
+         ihaskell-hvega = nixpkgsFor.${system}.ihaskell-hvega;
       });
+      # what do we want for the defaultPackage here?
       defaultPackage = forAllSystems (system: self.packages.${system}.hvega);
       checks = self.packages;
       devShell = forAllSystems (system: let haskellPackages = nixpkgsFor.${system}.haskellPackages;
         in haskellPackages.shellFor {
-          packages = p: [self.packages.${system}.hvega];
+          packages = p: [self.packages.${system}.hvega
+	                 self.packages.${system}.ihaskell-hvega];
           # withHoogle = true;
-          withHoogle = false;
+	  withHoogle = false;  # faster ...
           buildInputs = with haskellPackages; [
             haskell-language-server
             hlint
             cabal-install
+	    ihaskell
+	    nixpkgsFor.${system}.jupyter
           ];
         # Change the prompt to show that you are in a devShell
         shellHook = ''
-  echo -e "*** \e[1;32mWelcome to hvega\e[0m ***"
+  echo -e "*** \e[1;32mWelcome to hvega development\e[0m ***"
   ghc --version
   cabal --version
   hlint --version
+  echo -e "IHaskell: `ihaskell --version`"
   echo -e ""
-  export PS1='hvega:\A \e[1;34m\w\e[0m '
+  export PS1='hvegadev:\A \e[1;34m\w\e[0m '
 	'';
         });
   };
